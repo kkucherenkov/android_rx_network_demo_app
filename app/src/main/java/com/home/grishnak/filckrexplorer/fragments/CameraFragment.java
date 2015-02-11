632f17e0477bc1dcf27f510f71c96093ce7c5615
Kirill Kucherenkov <kirill.kucherenkov@gmail.com>
2015-02-11 20:57:45 +0300

    remove unnecessary comments

--- app/src/main/java/com/home/grishnak/filckrexplorer/fragments/CameraFragment.java
+++ app/src/main/java/com/home/grishnak/filckrexplorer/fragments/CameraFragment.java
@@ -89,17 +89,4 @@ public class CameraFragment extends BaseFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-
-
 }
